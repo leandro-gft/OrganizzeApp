05da7522a6615461aa43b1b9bc579aca494c52af
leandro-gft <lpsacchi@gmail.com>
2020-06-08 18:54:49 -0300

    Retorna nome do usuario na tela principal

--- app/src/main/java/br/com/gft/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/br/com/gft/organizze/activity/PrincipalActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import br.com.gft.organizze.R;
+import br.com.gft.organizze.helper.Base64Custom;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -20,7 +28,7 @@ public class PrincipalActivity extends AppCompatActivity {
     private Button btnSignOut;
     private FloatingActionButton fabD, fabR;
     private FirebaseAuth auth = FirebaseAuth.getInstance();
-//    private DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +40,21 @@ public class PrincipalActivity extends AppCompatActivity {
         txtWelcome = findViewById(R.id.textWelcome);
         btnSignOut = findViewById(R.id.btnSignOut);
 
-//        txtWelcome.setText("Olá " + db.child("usuarios").child(
-//                Base64Custom.codificarBase64(auth.getCurrentUser().getEmail())));
+        String emailCodificado = auth.getCurrentUser().getEmail();
+        referencia.child("usuarios")
+                .child(Base64Custom.codificarBase64(emailCodificado))
+                .child("nome")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String nome = dataSnapshot.getValue().toString();
+                        txtWelcome.setText("Olá " + nome);
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(PrincipalActivity.this, "Erro ao recuperar nome do usuário: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
